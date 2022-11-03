3ca7fceff0c35f24588233ade47c6f9ccbd787a5
박찬섭 <cksk49@naver.com>
2022-11-03 19:49:46 +0900

    최소, 최대

---
+++ 백준배열문제/work3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+int a = 0;
+scanf("%d",&a);
+int arr[a];
+for(int i=0; i<a; i++) {
+	scanf("%d",&arr[i]);
+}
+
+for(int i = 0; i<a; i++){
+	for(int j = i+1; j<a; j++) {
+		if(arr[i]>arr[j]){
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp; 
+		}
+	}
+}
+
+printf("%d %d",arr[0], arr[a-1]);
+
+return 0;
+}
