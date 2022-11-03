30512913e1703fd180b4fd583084865d56f3b7d0
박찬섭 <cksk49@naver.com>
2022-11-03 19:02:11 +0900

    배열첫문제

---
+++ 백준배열문제/work1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main() {
+
+int a = 0;
+
+scanf("%d",&a);
+
+int arr[a];
+
+for(int i = 0; i<a; i++) {
+	scanf("%d",&arr[i]);
+}
+
+int b = 0;
+scanf("%d",&a);
+int c = 0;
+
+for(int i =0; i<a; i++)  {
+	if(arr[i] == b) {
+		c +=1;	
+	}
+
+}
+printf("%d",c);
+
+return 0;
+}
