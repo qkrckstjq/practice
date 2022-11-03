b1b0b0cbaaad1e43126ddeef3ce7ce52e5966779
박찬섭 <cksk49@naver.com>
2022-11-03 21:32:53 +0900

    변수이름오류

--- 백준배열문제/work5.c
+++ 백준배열문제/work5.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main() {
-	int class[30] = {0};
+	int a[30] = {0};
 	int sub[28];
 	
-	for(int i = 0; i<sizeof(class); i++) {
-		class[i] = i+1;
+	for(int i = 0; i<sizeof(a); i++) {
+		a[i] = i+1;
 	}
 
 	for(int i = 0; i<sizeof(sub); i++) {
 		scanf("%d",&sub[i]);
 	}
 	
-	for(int i = 0; i<sizeof(class); i++) {
+	for(int i = 0; i<sizeof(a); i++) {
 		for(int j = 0; j<sizeof(sub); j++){
-			if(class[i] == sub[j]){
-				class[i] = 0;
+			if(a[i] == sub[j]){
+				a[i] = 0;
 			}
 		}
 	}
 	
-	for(int i = 0; i<sizeof(class); i++) {
-		if(class[i] != 0){
-			printf("%d",class[i]);
+	for(int i = 0; i<sizeof(a); i++) {
+		if(a[i] != 0){
+			printf("%d",a[i]);
 		}
 	
 	}
