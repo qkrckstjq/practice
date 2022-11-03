c4a13f5c57224fac71f36b031f690e4d70efc5dc
박찬섭 <cksk49@naver.com>
2022-11-03 21:38:16 +0900

    배열빈곳찾기수정본

--- 백준배열문제/work5.c
+++ 백준배열문제/work5.c
@@ -4,23 +4,23 @@ int main() {
 	int a[30] = {0};
 	int sub[28];
 	
-	for(int i = 0; i<sizeof(a); i++) {
+	for(int i = 0; i<sizeof(a)/4; i++) {
 		a[i] = i+1;
 	}
 
-	for(int i = 0; i<sizeof(sub); i++) {
+	for(int i = 0; i<sizeof(sub)/4; i++) {
 		scanf("%d",&sub[i]);
 	}
 	
-	for(int i = 0; i<sizeof(a); i++) {
-		for(int j = 0; j<sizeof(sub); j++){
+	for(int i = 0; i<sizeof(a)/4; i++) {
+		for(int j = 0; j<sizeof(sub)/4; j++){
 			if(a[i] == sub[j]){
 				a[i] = 0;
 			}
 		}
 	}
 	
-	for(int i = 0; i<sizeof(a); i++) {
+	for(int i = 0; i<sizeof(a)/4; i++) {
 		if(a[i] != 0){
 			printf("%d",a[i]);
 		}
