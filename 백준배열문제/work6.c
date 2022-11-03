4a277be328ddf1ec2e14d7b17f7897d8abefcf31
박찬섭 <cksk49@naver.com>
2022-11-04 00:05:13 +0900

    배열안에서 중복되는것 제거

--- 백준배열문제/work6.c
+++ 백준배열문제/work6.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 
 int main() {
-	int a[10];
-	for(int i = 0; i < 10; i++) {
-		scanf("%d",&a[i]);
+	int a[10] = { 0 };
+	int num[10];
+	for (int i = 0; i < 10; i++) {
+		scanf_s("%d", &a[i]);
 	}
-
-	for(int i = 0; i<10; i++) {
+	for (int i = 0; i < 10; i++) {
 		a[i] = a[i] % 42;
+		num[i] = 42;
 	}
-	
-	int same[10] = {42};	
-	int d = 0;
-	int ten = 10;
 
-	for(int i = 0; i<10; i++) {
-		for(int j = i+1; j<10; j++) {
-			if(a[i] == a[j]) {
-				same[d] = a[j];
-				d++;
+	for (int i = 0; i < 10; i++) {
+		for (int j = i+1; j < 10; j++) {
+			if (a[i] == a[j]) {
+				num[i] = a[i];
 			}
 		}
 	}
-	
-	for(int i = 0; i<10; i++) {
-		if(same[i] != 42) {
-			ten -=1;
+
+	int ten = 10;
+
+	for (int i = 0; i < 10; i++) {
+		if (num[i] != 42) {
+			ten -= 1;
 		}
 	}
 
-	printf("%d",ten);
+	printf("%d", ten);
 
 }
