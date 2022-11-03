9860e0b3f67f88f143a8c7c56f4a3b89258762e8
박찬섭 <cksk49@naver.com>
2022-11-03 21:03:30 +0900

    기존 이중for문은 배열크기가 커지면 연산이 길어짐, 한개for으로수정

--- 백준배열문제/work3.c
+++ 백준배열문제/work3.c
@@ -7,17 +7,20 @@ int arr[a];
 for(int i=0; i<a; i++) {
 	scanf("%d",&arr[i]);
 }
+int min = arr[0];
+int max = arr[0];
+int sample = 0;
+for(int i = 0; i<a; i++) {
 
-for(int i = 0; i<a; i++){
-	for(int j = i; j<a; j++) {
-		if(arr[i]>arr[j]){
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp; 
-		}
+	if(min > arr[i]){
+		min = arr[i]; 
+	}
+	if(max < arr[i]){
+		max = arr[i];
 	}
 }
-printf("%d %d",arr[0], arr[a-1]);
+
+printf("%d %d", min, max);
 
 return 0;
 }
