7c2a654f8ab4f80207c340db5ba314c718e3b868
박찬섭 <cksk49@naver.com>
2022-11-08 00:42:21 +0900

    배열안에서 평균을구하고 그 평균값이상의 퍼센테이지표현

--- 백준배열문제/work9.c
+++ 백준배열문제/work9.c
@@ -1,41 +1,84 @@
 #include <stdio.h>
-
-
+ 
+ 
 int main() {
-	int a = 0;
-	scanf("%d",&a);
-	
-	int b;
-	int c;
-
-	int arr[a][b][c];
-	
-	for(int i = 0 ; i < a; i++) {
-		scanf("%d",&b);	
-		for(int j =0; j<b; j++) {
-			scanf("%d",&c);
-		}
-	}
-
-	float avg[a];
-
-	for(int i = 0 ; i < a; i++) {
-
-		int num = 0;
-		for(int j = 0; j<b; j++) {
-			for(int k = 0 ; k < c; k++) {
-				avg[i] += arr[i][j][k];
-			}
-		num = j;
-		}
-		
-	avg[i] = avg[i] / num;
-	}
-
-	
-	for(int i = 0; i<a; i++) {
-		
-		printf("%f.3", avg[i]);
-	}
-
+    int a; 
+    scanf("%d",&a);
+    
+    int arr[a][1001];
+    float num[a];
+    float sum[a];
+    float avg[a];
+    
+    float g[a]; //각 케이스 학생수 
+    float h[a]; //각 케이스에서 평균을 넘는 학생수
+    
+    for(int i = 0; i<a; i++){
+        num[i] = 0;
+        sum[i] = 0;
+        avg[i] = 0;
+        g[i] = 0;
+        h[i] = 0;
+        for(int j = 0; j<1001; j++){
+            arr[i][j] = 111;
+        }
+        
+    }
+    
+    for(int i = 0; i < a; i++){
+        int b = 0;
+        scanf("%d",&b);
+        g[i] = b;
+        num[i] = b;
+        for(int j = 0; j < b; j++){
+            int c = 0;
+            scanf("%d",&c);
+            arr[i][j] = c;
+            sum[i] += c;
+        }
+    }
+    
+    for(int i = 0; i < a; i++) {
+        avg[i] = sum[i] / num[i];
+    }
+    
+    for(int i = 0; i < a; i++) {
+        int q = 0;
+        int n = 0;
+        float w = 0;
+        while(arr[i][n] != 111){
+            
+            if(avg[i] < arr[i][n]) {
+                w += 1;
+                h[i] = w;
+            }
+            n += 1;
+        }
+    }
+    
+    for(int i = 0; i < a; i++) {
+        printf("%.3f", (h[i] / g[i]) * 100);
+        printf("%%");
+        printf("\n");
+    }
+    
+    /*int d = 0;
+    int e = 0;
+    
+    while(0 != arr[d][e]) {
+        printf("%d ",arr[d][e]);
+        e++;
+        
+        if(0 == arr[d][e]){
+            printf("\n");
+            d +=1;
+            e = 0;
+        }
+    }*/
 }
+
+
+
+
+
+
