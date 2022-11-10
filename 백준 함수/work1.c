f80f0c1dd231922e07b2cefb55bb1fe97e881479
박찬섭 <cksk49@naver.com>
2022-11-10 20:41:50 +0900

    답은맞는거같은데 출력초과뜸

--- 백준 함수/work1.c
+++ 백준 함수/work1.c
@@ -25,7 +25,7 @@ while(b < size) {
 
         for(int i = 0 ; i < size; i++){
                 if(b == arr[i]) {
-                        arr[i] = 0;
+                        arr[i] = -1;
                 }
         }
 }
@@ -33,8 +33,8 @@ while(b < size) {
 int num = 0;
 
 while(num < size) {
-	if(arr[num] != 0) {
-		printf("%d",arr[num]);
+	if(arr[num] != -1) {
+		printf("%d\n",arr[num]);
 	}
     	
         num++;
