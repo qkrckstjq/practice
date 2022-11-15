d37b84733b961292f8744a230803c0dcfdd362bd
박찬섭 <cksk49@naver.com>
2022-11-15 18:44:16 +0900

    문자열 아스키코드

--- 백준 문자열/work1.c
+++ 백준 문자열/work1.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 
-int stn(char str[]) {
-	int arr[sizeof(str)] = {0};
-	for (int i = 0; i < sizeof(str); i++) {
-		arr[i] = str[i];
-		arr[i] = arr[i] - 48;
-	}
-	int sum = 0;
-	for (int i = 0; i < sizeof(str); i++) {
-		sum += arr[i];
-	}
-	return sum;
-}
 
 int main() {
-
+	char a;
+	scanf("%c",&a);
+	printf("%d",a);
 }
--- 백준 문자열/work2.c
+++ 백준 문자열/work2.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-
-int main () { 
-	int a = 0;
-	char b;
-	scanf("%d %c",&a,&b);	
-	
-
-	for(int i=0; i<a; i++) { 
-		b(i);
-		printf("\n");
+int stn(char str[]) {
+	int arr[sizeof(str)] = {0};
+	for (int i = 0; i < sizeof(str); i++) {
+		arr[i] = str[i];
+		arr[i] = arr[i] - 48;
 	}
+	int sum = 0;
+	for (int i = 0; i < sizeof(str); i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
+int main() {
 
 }
