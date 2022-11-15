54419faccd39a09b377df39ea6252d260a2f755f
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2022-11-15 18:37:26 +0900

    Add files via upload

--- 백준 문자열/work1.c
+++ 백준 문자열/work1.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+int stn(char str[]) {
+	int arr[sizeof(str)] = {0};
+	for (int i = 0; i < sizeof(str); i++) {
+		arr[i] = str[i];
+		arr[i] = arr[i] - 48;
+	}
+	int sum = 0;
+	for (int i = 0; i < sizeof(str); i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
 
-int main () {
-	char a;	
+int main() {
 
-	scanf("%c",&a);
-
-	printf("%d",a);
 }
