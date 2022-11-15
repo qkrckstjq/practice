300f37193b86b4bc2bdd265f03324c948aee2918
박찬섭 <cksk49@naver.com>
2022-11-16 00:36:26 +0900

    문자열 찾기

--- 백준 문자열/work3.c
+++ 백준 문자열/work3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	char a[] = "";
+	char a[100];
 	scanf("%s",a);
 	
 	int str_len = 0;
@@ -10,28 +10,28 @@ int main() {
 		str_len++;
 	}
 	int c = 0;
-	char alphabet[27] = "";
+	char alphabet[26] = "";
 	for(int i = 97; i <= 122; i++) {
 		alphabet[c] = i; 
 		c++;
 	}
 	
-	int arr[27] = {0};
+	int arr[26] = {0};
 	
-	for(int i = 0; i<27; i++) {
+	for(int i = 0; i<26; i++) {
 		arr[i] = -1;
 	}
 
-	
-	for(int i = 0; i<str_len+1; i++) {	
-		for(int j = 0; j < 27; j++) {
+	for(int i = 0; i<str_len; i++) {	
+		for(int j = 0; j < 26; j++) {
 			if(a[i] == alphabet[j]) {
 				arr[j] = i;
+				alphabet[j] = 0;
 			}
 		}
 	}
 	
-	for(int i = 0; i<27; i++) {
+	for(int i = 0; i<26; i++) {
 		printf("%d ",arr[i]);
 	}
 
