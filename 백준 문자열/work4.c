fc23e43a836c57663b0fec4f155e5eba44286942
박찬섭 <cksk49@naver.com>
2022-11-17 17:20:42 +0900

    문자열 반복

---
+++ 백준 문자열/work4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main () {
+	int a =0;
+	scanf("%d",&a);
+
+	int arr[a]={0};
+	char str[a][21] = {0};
+	
+	for(int i = 0; i < a; i++) {
+		scanf("%d",&arr[i]);
+		scanf("%s", str[i]);
+	}
+
+	for(int i = 0; i < a; i++) {
+		int len = 0;
+		while(str[i][len]) {
+			len++;
+		}
+		
+		for(int j = 0; j < len; j++) {
+			for(int k = 0; k < arr[i]; k++) {
+				printf("%c",str[i][j]);
+			}
+		
+		}
+		
+		printf("\n");
+	}
+
+}
