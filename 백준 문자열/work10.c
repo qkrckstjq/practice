0d9b3d8bcccc01700ee354a9c3c739244b2bc94b
박찬섭 <cksk49@naver.com>
2022-11-19 00:07:48 +0900

    그룹단어체커

---
+++ 백준 문자열/work10.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+int main () {
+	int a =0 ;
+	scanf("%d",&a);
+	int ans = a;
+
+
+	char str[a][100];
+	for(int i = 0; i < a; i++) {
+		scanf("%s",str[i]);
+	} 
+
+
+	for(int i = 0; i < a; i++) {
+		int len = 0;
+		while(str[i][len]) len++;
+		int arr[len];
+		for(int j = 0; j < len; j++) {
+			arr[j] = 0;
+		}	
+
+		for(int j = 0; j <= len; j++) {
+			for(int k = j; k <= len; k++) {
+				if(str[i][j] == str[i][k]) {
+					arr[j] = str[i][k];
+				} else if (str[i][j] != str[i][k]){
+					arr[j+1] = str[i][k];
+					j++;
+				}
+			}
+		}
+		int bool = 1;
+		int newlen = 0;
+		while(arr[newlen]) newlen++;
+		
+		for(int j = 0; j<=newlen; j++) {
+			for(int k = j+1; k<=newlen; k++) {
+				if(arr[j] == arr[k]) {
+					bool *= 0;
+				}
+			}
+		}
+		
+		if(bool == 0) {
+			ans-=1;
+		}		
+
+	}
+	
+	printf("%d",ans);
+
+
+	
+
+
+}
