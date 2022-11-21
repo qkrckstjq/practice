af7b6592ac63ef73ef7531110b01cd4d2d1e416d
박찬섭 <cksk49@naver.com>
2022-11-21 23:18:59 +0900

    반복문사용시 최대21억번까지계산하여 계산량이너무많아짐 일반식을사용하여 구함

--- 백준 기본수학1/work1.c
+++ 백준 기본수학1/work1.c
@@ -7,19 +7,15 @@ int main () {
 	long long c=0;
 	scanf("%lld %lld %lld",&a,&b,&c);
 	
-	long long B = b;
-	long long C = c;	
-	long long d = 1;
-	while(1) {
-		if(c < b) {
-			d = -1;
-			break;
-		}
-		B=b*d;
-                C=c*d;		
-		if(a+B < C) break;
-		d+=1;
-	}
+	long long d = 0;
+	
+	if(c<=b) {
+		d = -1;	
+	} else {
+		d = (a/(c-b)) + 1;
+	}	
+
+
 
 	printf("%lld",d);
 
