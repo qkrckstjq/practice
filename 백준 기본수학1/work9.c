b19c6c2733b43e48909525c1e13656de6598f17e
박찬섭 <cksk49@naver.com>
2022-11-24 18:36:20 +0900

    자료형을 벗어나는 큰 수 더하기

---
+++ 백준 기본수학1/work9.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+
+int main() {
+	char a[10001],b[10001];
+	scanf("%s %s",a,b);
+	int alen = 0, blen = 0, bigger = 0;
+	while(a[alen]) alen++;
+	while(b[blen]) blen++;
+	if(alen >= blen) {
+		bigger = alen+1;
+	} else {
+		bigger = blen+1;
+	}
+	int arr[bigger];
+	for(int i = 0; i < bigger; i++) {
+		arr[i]=-1;
+	}
+	int extra=0;
+	for(int i = (bigger-1); i>=0; i--) {
+		arr[i] = ((a[alen-1] - 48) + (b[blen-1] - 48)) % 10 + extra;
+		
+		
+		extra = ((a[alen-1] - 48) + (b[blen-1] - 48)) / 10;
+		if((alen-1) == 0) {
+			a[alen-1] = 48;
+		} else {
+			alen--;
+		}
+		if((blen-1) == 0) {
+			b[blen-1] = 48;
+		} else {
+			blen--;
+		}		
+	}
+	int c = 0;
+	int d = 0;
+	for(int i = 0; i<bigger; i++) {
+		if(arr[i] != 0) {
+			d = i;
+			break;
+		}
+	}	
+	
+
+	for(int i = d; i<bigger; i++) {
+		printf("%d",arr[i]);
+	}
+	
+
+}
